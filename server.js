ba09a25004ff417961c64641a5ed20f736a5af29
Antony-AK <antonyak147@gmail.com>
2026-01-30 22:23:07 +0530

    Port updated

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ import aiRoutes from "./routes/ai.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: ["https://ai-counsellor-murex.vercel.app/"],
+  origin: "*",
   credentials: true
 }));
+
 app.use(express.json());
 
 connectDB();
@@ -30,6 +31,8 @@ app.get("/", (req, res) => {
   res.send("AI Counsellor API is running 🚀");
 });
 
-app.listen(5000, () => {
-    console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port", PORT);
 });
