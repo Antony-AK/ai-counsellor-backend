69e4dcee84874b96efb89dea544fb24723394ef7
Antony-AK <antonyak147@gmail.com>
2026-01-30 22:05:20 +0530

    Frontend Updated

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ import aiRoutes from "./routes/ai.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://ai-counsellor-murex.vercel.app/"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
